3ff5625791013b300ceac32f89efde08b86fc0e6
SNEHA BILLA <139547086+BillaSneha3@users.noreply.github.com>
2023-08-19 15:42:15 +0530

    Add files via upload

---
+++ zerodimensions.py
@@ -0,0 +1,33 @@
+import numpy as np
+zerodimension=np.array(10)
+zerodimension1=np.array(20)
+#adding zero dimension arrays
+addresult=zerodimension+zerodimension1
+print("addition of two zerodimension arrays ",addresult)
+
+#subtracting zero dimension arrays
+subresult=zerodimension-zerodimension1
+print("subtraction of two zerodimension arrays ",subresult)
+
+#multiplying zero dimension arrays
+mulresult=zerodimension*zerodimension1
+print("multiplication of two zerodimension arrays ",mulresult)
+
+#division zero dimension arrays
+divisionresult=zerodimension/zerodimension1
+print("division of two zerodimension arrays ",divisionresult)
+
+#floordivision of zero dimension arrays
+floorresult=zerodimension//zerodimension1
+print("floorDivision of two zerodimension arrays ",floorresult)
+
+#modulus of  zero dimension arrays
+modresult=zerodimension%zerodimension1
+print("modulus of two zerodimension arrays ",modresult)
+
+
+
+
+
+
+
